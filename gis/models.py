6d3650baf0b55d18abb1de0bc6d8fc8ae0289ddb
OleksandrStoliar <olstoliar99@gmail.com>
2019-03-13 13:36:58 +0200

    First template Home was created

--- gis/models.py
+++ gis/models.py
@@ -4,12 +4,15 @@ from __future__ import unicode_literals
 from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
 class Articles(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
     user = models.ForeignKey(User)
 
+
     def __str__(self):
-        pass
+        return self.title
+
 
