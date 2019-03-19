b4dafc8d40dddd416fac81996927d8253d425468
OleksandrStoliar <olstoliar99@gmail.com>
2019-03-19 12:53:55 +0200

    Article...

--- gis/views.py
+++ gis/views.py
@@ -3,8 +3,19 @@ from __future__ import unicode_literals
 
 from django.http import HttpResponse
 from django.shortcuts import render
+from gis.models import Articles
 
 # Create your views here.
 def home(request):
+
     # return HttpResponse("Hello world")
-    return render(request, 'gis/home.html')
+    #return render(request, 'gis/home.html')
+    articles = Articles.objects.all()
+    context = {
+        'articles': articles
+    }
+    return render(request, 'gis/home.html', context)
+
+
+def about(request):
+    return render(request, 'gis/about.html')
